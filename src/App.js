e0cdef56aa400f855a72d8d9c5e70a1147cbad61
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2025-10-15 13:58:52 -0300

    feat: add functions in buttons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import Button from "./components/Button";
 import { useState } from "react";
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState("0");
+  const [currentNumber, setCurrentNumber] = useState(0);
+
+  const handleAddNumber = (number) => {
+    if(currentNumber === 0) {
+      setCurrentNumber(number);
+      return;
+    }
+    setCurrentNumber(prev => `${prev}${number}`);
+  }
+
+  const handleClear = () => {
+    setCurrentNumber(0);
+  }
+
+
 
   return (
     <Container>
@@ -12,28 +26,28 @@ const App = () => {
       <Content>
         <Input value={currentNumber} />
         <Row>
-            <Button label="x" />
-            <Button label="÷" />
-            <Button label="( )" />
-            <Button label="C" />
+            <Button label="x" onClick={() =>handleAddNumber(" x ")} />
+            <Button label="÷" onClick={() =>handleAddNumber(" ÷ ")} />
+            <Button label="( )" onClick={() =>handleAddNumber("")} />
+            <Button label="C" onClick={() => handleClear() } />
         </Row>
         <Row>
-            <Button label="7" />
-            <Button label="8" />
-            <Button label="9" />
-            <Button label="-" />
+            <Button label="7" onClick={() =>handleAddNumber("7")} />
+            <Button label="8" onClick={() =>handleAddNumber("8")} />
+            <Button label="9" onClick={() =>handleAddNumber("9")} />
+            <Button label="-" onClick={() =>handleAddNumber(" - ")} />
         </Row>
         <Row>
-            <Button label="4" />
-            <Button label="5" />
-            <Button label="6" />
-            <Button label="+" />
+            <Button label="4" onClick={() =>handleAddNumber("4")} />
+            <Button label="5" onClick={() =>handleAddNumber("5")} />
+            <Button label="6" onClick={() =>handleAddNumber("6")} />
+            <Button label="+" onClick={() =>handleAddNumber(" + ")} />
         </Row>
         <Row>
-            <Button label="1" />
-            <Button label="2" />
-            <Button label="3" />
-            <Button label="=" />
+            <Button label="1" onClick={() =>handleAddNumber("1")} />
+            <Button label="2" onClick={() =>handleAddNumber("2")} />
+            <Button label="3" onClick={() =>handleAddNumber("3")} />
+            <Button label="=" onClick={() =>handleAddNumber("")} />
         </Row>
       </Content>
     </Container>
